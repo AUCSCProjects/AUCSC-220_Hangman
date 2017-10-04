60e12371265b3139a9e4f4a1111bd72a2965b23e
Mark Lisoway <AUCSCProjects>
2017-10-03 20:39:39 -0600

    Applied basic fullscreen parameters

--- app/src/main/java/hangman/groupone/projects/aucsc/hangman/Main.java
+++ app/src/main/java/hangman/groupone/projects/aucsc/hangman/Main.java
@@ -2,12 +2,20 @@ package hangman.groupone.projects.aucsc.hangman;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Main extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        applyWindowSettings();
         setContentView(R.layout.activity_main);
+    }
+
+    private void applyWindowSettings() {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
